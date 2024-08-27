6200cfc5137d5addcf434f184fe0a45e58ff4aa3
hibamroum <hibamroumani@gmail.com>
2024-08-27 12:03:50 +0300

    finalized needed configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+//tsx refers to React components while ts refers to normal typescript files
+
+
+//import Navbar from "@scenes/Na"
+function App() {
+  return <div >
+    Hello World!
+  </div>;
+}
+
+
+export default App;
