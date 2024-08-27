6453daaadbc6ba0e51c983004e9976ef0c5c1643
hibamroum <hibamroumani@gmail.com>
2024-08-27 18:02:37 +0300

    navbar for mobile screens

--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -73,6 +73,43 @@ function Navbar({ selectedPage, setSelectedPage }: Props) {
           )}
         </div>
       </div>
+      {/*Mobile Menu */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/*Close Icon */}
+          <div className="flex justify-end p-12">
+            <button
+              onClick={() => {
+                setIsMenuToggled(!isMenuToggled);
+              }}
+            >
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
